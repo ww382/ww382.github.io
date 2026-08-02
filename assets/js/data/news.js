21ffde949ab47e7159f30c9426034f36dfde6150
Wensi Wu <ww382@cornell.edu>
2026-08-02 15:02:47 -0400

    Homepage spacing tweak, about-me copy simplified
    - Widen the gap between the homepage photo and bio text (text column
      narrowed; since the layout right-aligns the text via
      justify-content:space-between, this shifts its left edge right
      without moving the photo).
    - Rewrite the about-me bio into shorter, more direct sentences and
      trim a redundant phrase in one news item.
    - Minor grammar fix: "NHLBI funded" -> "NHLBI-funded".

--- assets/js/data/news.js
+++ assets/js/data/news.js
@@ -1,12 +1,12 @@
 // News feed entries for /about_me/. To add an entry, add one object here
 // (newest first -- renderNews() preserves array order, it does not sort).
 var NEWS_ITEMS = [
-  { date: "Mar 30, 2026", html: 'My team received a Cardiac Center Innovation Award from the <a href="https://www.chop.edu/centers-programs/cardiac-center" target="_blank"><u>Children\'s Hospital of Philadelphia Cardiac Center</u></a>!' },
+  { date: "Mar 30, 2026", html: 'My team received an Innovation Award from <a href="https://www.chop.edu/centers-programs/cardiac-center" target="_blank"><u>CHOP\'s Cardiac Center</u></a>!' },
   { date: "Jul 1, 2025", html: 'My team received an independent investigator research award from the <a href="https://www.additionalventures.org/funding/single-ventricle-research-fund/" target="_blank"><u>Additional Ventures Single Ventricle Research Fund</u></a>!' },
   { date: "Jun 20, 2025", html: 'My team received computational credits from <a href="https://access-ci.org/" target="_blank"><u>the ACCESS program</u></a> funded by the National Science Foundation!' },
   { date: "Jun 16, 2025", html: 'My feature piece was published on the <a href="https://blog.seas.upenn.edu/wensi-wu-uses-digital-twins-to-explore-the-hidden-mechanics-of-the-human-heart/" target="_blank"><u>Penn Engineering blog</u></a>!' },
   { date: "Aug 31, 2023", html: "I received an NIH/NHLBI K25 Mentored Quantitative Research Career Development Award!" },
-  { date: "Jun 1, 2022", html: "I received an NHLBI funded CHOP Cardiology T32 fellowship!" },
+  { date: "Jun 1, 2022", html: "I received an NHLBI-funded CHOP Cardiology T32 fellowship!" },
   { date: "Apr 26, 2021", html: "I successfully defended my Ph.D. dissertation!" },
   { date: "Apr 19, 2020", html: 'We released a new version of <a href="https://github.com/nonlinearfun/CU-BENs/releases/tag/v4.1.2" target="_blank"><b><u>CU-BENs</u></b></a>!' },
   { date: "May 15, 2019", html: 'Our <a href="https://www.youtube.com/watch?v=CAjho-7CQwc" target="_blank"><b><u>video</u></b></a> took second place in the <b>CEE Video Contest</b> organized by the Cornell CEE GSA!' }
